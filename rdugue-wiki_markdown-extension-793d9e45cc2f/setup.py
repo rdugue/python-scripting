9a2fc1e74adf807225d36c24490c4ea10230991c
dugueR <rdugue@telcosupport.net>
2015-09-21 03:30:35 -0400

    Markdown Extension Upload

---
+++ rdugue-wiki_markdown-extension-793d9e45cc2f/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name='wiki_markdown',
+    version='1.0',
+    description='Markdown extension for AfricanaWiki',
+    author='Ralph Dugue',
+    author_email='rdugue1@gmail.com',
+    url='https://rdugue@bitbucket.org/rdugue/',
+    py_modules=['wiki_markdown'],
+    install_requires = ['markdown>=2.5'],
+)
